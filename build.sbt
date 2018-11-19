f34a6417a7a22b0b7828c5707bd9f53cded4d875
dershovGD <dershov@griddynamics.com>
2018-11-19 20:11:28 +0300

    Add posibility to create fat jars

--- build.sbt
+++ build.sbt
@@ -11,4 +11,9 @@ libraryDependencies += "org.scalactic" %% "scalactic" % "3.0.5"
 libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.5" % "test"
 libraryDependencies += "commons-net" % "commons-net" % "3.6"
 libraryDependencies += "org.apache.spark" %% "spark-hive" % "1.6.0"
-libraryDependencies += "com.h2database" % "h2" % "1.4.192"
+libraryDependencies += "com.h2database" % "h2" % "1.4.192" % "test"
+
+//assemblyMergeStrategy in assembly := {
+// case PathList("META-INF", xs @ _*) => MergeStrategy.discard
+// case x => MergeStrategy.first
+//}
--- src/main/scala/top_categories/TopNCategoriesDF.scala
+++ src/main/scala/top_categories/TopNCategoriesDF.scala
@@ -1,9 +1,10 @@
 package top_categories
 
+import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.functions.desc
 import org.apache.spark.sql.hive.HiveContext
-import utils.InputProcessor
+import utils.{DBOutputWriter, InputProcessor}
 
 class TopNCategoriesDF(private val hiveContext : HiveContext) {
   def calculateUsingDF(inputFile: String, n: Int): DataFrame = {
@@ -12,6 +13,29 @@ class TopNCategoriesDF(private val hiveContext : HiveContext) {
       count().
       sort(desc("count")).
       limit(n)
+  }
+
+}
+
+object TopNCategoriesDF {
+  def main(args : Array[String]) : Unit = {
+    val dataFrame = calculate(args)
+    val prop = new java.util.Properties
+    prop.setProperty("driver", "com.mysql.jdbc.Driver")
+    prop.setProperty("user", "root")
+    prop.setProperty("password", "cloudera")
+    val url = "jdbc:mysql://localhost:3306/hadoop"
+    val tableName = "spark_top_categories_df"
+    new DBOutputWriter(prop, url, tableName).writeDataFrame(dataFrame)
+  }
+
+  def calculate(args : Array[String]) : DataFrame = {
+    val conf = new SparkConf()
+      .setMaster("local[3]")
+      .setAppName("TopNCategoriesDF")
+    val sc = new SparkContext(conf)
+
+    new TopNCategoriesDF(new HiveContext(sc)).calculateUsingDF(args(0), 10)
   }
 
 }
