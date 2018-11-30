953fb5c5b6f749380c647db9f0d5b8da2345cbf1
dershovGD <dershov@griddynamics.com>
2018-11-30 20:38:00 +0300

    Optimize join in TopSpendingCountriesDF

--- src/main/scala/top_spending_countries/TopNSpendingCountriesDF.scala
+++ src/main/scala/top_spending_countries/TopNSpendingCountriesDF.scala
@@ -1,30 +1,34 @@
 package top_spending_countries
 
-import org.apache.commons.net.util.SubnetUtils
 import org.apache.spark.sql.DataFrame
-import org.apache.spark.sql.functions.{broadcast, desc, udf}
+import org.apache.spark.sql.functions._
 import org.apache.spark.sql.hive.HiveContext
-import utils.{Calculator, Runner, SchemaManager}
+import utils._
 
 class TopNSpendingCountriesDF(private val inputFiles: Array[String]) extends Calculator{
   val inputPurchases = inputFiles(0)
   val inputCountries = inputFiles(1)
   def   calculateUsingDF(hiveContext: HiveContext, n: Int): DataFrame = {
     val schemaManager = new SchemaManager(hiveContext)
-    val isInRange = udf((network: String, ip: String) => new SubnetUtils(network).getInfo.isInRange(ip))
-    val events = schemaManager.createEventsDF(inputPurchases) 
-    val countries = schemaManager.createCountriesDF(inputCountries) //m*logm
 
-    events.join(broadcast(countries), isInRange(countries("network"), events("ip_address"))). //n*m
+    val events = schemaManager.createEventsDF(inputPurchases)
+
+    val processor = new InputProcessor(hiveContext.sparkContext)
+    val networkCountries = processor.
+      readCountries(inputCountries).
+      map(country => NetworkCountry(country.network, country.countryName))
+    val countriesIpBroadcasted = hiveContext.sparkContext.broadcast(networkCountries)
+    val isInRange = udf((ip: String) => {
+      new CountryByIpFinder(countriesIpBroadcasted.value).findCountryByIp(ip).orNull
+    })
+
+    events.withColumn("country_name", isInRange(events("ip_address"))).
+      filter(col("country_name").isNotNull).
       groupBy("country_name").
       sum("product_price").
       withColumnRenamed("sum(product_price)", "total_purchases").
       sort(desc("total_purchases")).
       limit(n)
-      //(ipmin1, ipmax1) => (nmin1<ipmin1, nmax1> ipmax1)
-      //countries broadcast
-      //(ipmin, ipmax, country)
-
   }
 
   override def calculate(hiveContext: HiveContext, n: Int): DataFrame = {
